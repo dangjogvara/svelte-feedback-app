f3f2380bbb555c9befed50d611c2c920cbb49f15
Dan Poulsen <dangjogvara@gmail.com>
2022-04-22 21:02:27 +0100

    project start

--- src/App.svelte
+++ src/App.svelte
@@ -1,48 +1,9 @@
 <script>
-  let firstName = 'Dan';
-  let lastName = 'Poulsen';
-  let color = 'blue';
-  let showText = false;
-
-  $: name = firstName + ' ' + lastName;
-
-  const toggle = () => {
-    color = color === 'blue' ? 'red' : 'blue';
-    showText = !showText;
-  };
 </script>
 
 <main>
-  <h1 style="color: {color}">Hello {name}!</h1>
-  {#if showText}
-    <p>
-      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte
-      apps.
-    </p>
-  {:else}
-    <p>No text</p>
-  {/if}
-  <button on:click={toggle}>Click</button>
+  <h1>Hello</h1>
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
