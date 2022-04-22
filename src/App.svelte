62647b224374c9a8f7def4e1bfef037347ba9993
Dan Poulsen <dangjogvara@gmail.com>
2022-04-22 21:07:53 +0100

    dummy data

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,31 @@
 <script>
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: `Lorem ipsum 
+      dolor sit, amet consectetur 
+      adipisicing elit. Labore mollitia dolorem 
+      iste. Sapiente beatae deserunt similique hic tempore aperiam itaque molestiae, tenetur 
+      asperiores dignissimos ea. Aut deleniti accusamus itaque totam.`,
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: `Lorem ipsum, dolor sit amet consectetur 
+      adipisicing elit. Possimus, voluptates laboriosam eligendi nobis praesentium 
+      totam numquam? Voluptatum, libero molestias. Commodi neque dolorem ratione expedita tempore 
+      cupiditate nobis fuga repellat officiis.`,
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: `Lorem ipsum dolor, sit amet consectetur 
+      adipisicing elit. Deleniti eum quis vitae, at praesentium sint, 
+      voluptatum dolorem est a eos facilis beatae veritatis optio unde explicabo? 
+      Assumenda magni dolorem porro?`,
+    },
+  ];
 </script>
 
 <main>
