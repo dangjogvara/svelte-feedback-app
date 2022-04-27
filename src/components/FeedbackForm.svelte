4ce1b85b47c136ada6bb6f8f2343139c93368bd6
Dan Poulsen <dangjogvara@gmail.com>
2022-04-27 22:38:07 +0100

    format

--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -45,11 +45,13 @@
     max-width: 400px;
     margin: auto;
   }
+
   header h2 {
     font-size: 22px;
     font-weight: 600;
     text-align: center;
   }
+
   .input-group {
     display: flex;
     flex-direction: row;
@@ -58,14 +60,17 @@
     border-radius: 8px;
     margin-top: 15px;
   }
+
   input {
     flex-grow: 2;
     border: none;
     font-size: 16px;
   }
+
   input:focus {
     outline: none;
   }
+
   .message {
     padding-top: 10px;
     text-align: center;
